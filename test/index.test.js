0c4a2a97f32f13e8572549945cf076eb7e0ef71a
Greg Teixeira <greg.teixeira123@gmail.com>
2020-11-17 19:01:48 -0300

    added test for latin characters

--- test/index.test.js
+++ test/index.test.js
@@ -31,7 +31,7 @@ describe('Test 2', () => {
 	})
 })
 describe('Test 3', () => {
-	const str = 'consulting test'
+	const str = 'Consulting Teste'
 	const search = 's'
 	const parts = takeParts(str, search)
 	it('should have 2 highlights', () => {
@@ -58,5 +58,35 @@ describe('Test 4', () => {
 	})
 	it('should have length 11', () => {
 		expect(parts).toHaveLength(11)
+	})
+})
+describe('Test 5', () => {
+	const str = 'business'
+	const search = 's'
+	const parts = takeParts(str, search)
+	it('should have 3 highlights', () => {
+		expect(parts.filter(part => part.highlight)).toHaveLength(3)
+	})
+	it('should be equal original string', () => {
+		const final = parts.reduce((total, b) => total + b.text, '')
+		expect(final).toBe(str)
+	})
+	it('should have length 5', () => {
+		expect(parts).toHaveLength(5)
+	})
+})
+describe('Test 6', () => {
+	const str = 'São Paulo'
+	const search = 'sao'
+	const parts = takeParts(str, search)
+	it('should have 1 highlight', () => {
+		expect(parts.filter(part => part.highlight)).toHaveLength(1)
+	})
+	it('should be equal original string', () => {
+		const final = parts.reduce((total, b) => total + b.text, '')
+		expect(final).toBe(str)
+	})
+	it('should have length 2', () => {
+		expect(parts).toHaveLength(2)
 	})
 })
